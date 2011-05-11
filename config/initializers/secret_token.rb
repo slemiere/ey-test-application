67aab35f95236e7c00e8953bbe4946dc122dabda
Sylvain Lemiere <sylvain.lemiere@visuamobile.com>
2011-05-11 14:52:54 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyTestApplication::Application.config.secret_token = 'b3bbf6c2db175472c1b910894b91a3dd6463e9cf4421f5e63bbac0681aad61d6dead4a65d98ac162b572b5548fb338d4b2b228e10223d96620cb2f28a6aeb3de'
